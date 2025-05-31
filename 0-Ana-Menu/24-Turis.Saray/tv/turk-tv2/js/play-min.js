3add279ec83838e4a6aee594e096325dc60dba4a
Turis-Tv <turistv3@gmail.com>
2025-05-31 04:24:00 +0200

    Add files via upload

---
+++ 0-Ana-Menu/24-Turis.Saray/tv/turk-tv2/js/play-min.js
@@ -0,0 +1,275 @@
+<!--
+var win=null;
+
+onerror = stopError;
+function stopError(){
+	return true;
+}
+
+function TRT1(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tvcdn.4t40.workers.dev/https://trt.daioncdn.net/trt-1/master.m3u8?app=web","orta5",settings);
+	win.focus();
+}
+function ATV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://rnttwmjcin.turknet.ercdn.net/lcpmvefbyo/atv/atv.m3u8","orta5",settings);
+	win.focus();
+}
+function ATVAVRUPA(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://trkvz-live.daioncdn.net/atv/atv.m3u8","orta5",settings);
+	win.focus();
+}
+function STARTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://emmazbvkkf.turknet.ercdn.net/cnifuowzza/startv/startv.m3u8","orta5",settings);
+	win.focus();
+}
+function EUROSTAR(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://dogus-live.daioncdn.net/startv/playlist.m3u8","orta5",settings);
+	win.focus();
+}
+function FOX(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://uycyyuuzyh.turknet.ercdn.net/nphindgytw/nowtv/nowtv.m3u8","orta5",settings);
+	win.focus();
+}
+function KANALD(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://ackaxsqacw.turknet.ercdn.net/ozfkfbbjba/kanald/kanald.m3u8","orta5",settings);
+	win.focus();
+}
+function EUROD(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://demiroren-live.daioncdn.net/kanald/kanald.m3u8","orta5",settings);
+	win.focus();
+}
+function SHOWTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://rmtftbjlne.turknet.ercdn.net/bpeytmnqyp/showtv/showtv.m3u8","orta5",settings);
+	win.focus();
+}
+function SHOWTURK(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://ciner-live.daioncdn.net/showtv/showtv.m3u8","orta5",settings);
+	win.focus();
+}
+function TV8(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://rkhubpaomb.turknet.ercdn.net/fwjkgpasof/tv8/tv8.m3u8","orta5",settings);
+	win.focus();
+}
+function TV8INT(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://rkhubpaomb.turknet.ercdn.net/fwjkgpasof/tv8/tv8.m3u8","orta5",settings);
+	win.focus();
+}
+function TV85(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://rkhubpaomb.turknet.ercdn.net/fwjkgpasof/tv8bucuk/tv8bucuk.m3u8","orta5",settings);
+	win.focus();
+}
+function KANAL7(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://yurhnwtpys.turknet.ercdn.net/cvmbjbpmdx/kanal7/kanal7.m3u8","orta5",settings);
+	win.focus();
+}
+function A2TV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://rnttwmjcin.turknet.ercdn.net/lcpmvefbyo/a2tv/a2tv.m3u8","orta5",settings);
+	win.focus();
+}
+function BEYAZTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://beyaztv-live.daioncdn.net/beyaztv/beyaztv.m3u8","orta5",settings);
+	win.focus();
+}
+function TV360(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://turkmedya-live.ercdn.net/tv360/tv360.m3u8","orta5",settings);
+	win.focus();
+}
+function TEVE2(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://ackaxsqacw.turknet.ercdn.net/ozfkfbbjba/teve2/teve2.m3u8","orta5",settings);
+	win.focus();
+}
+function TRT2(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tv-trt2.medya.trt.com.tr/master.m3u8","orta5",settings);
+	win.focus();
+}
+function TRTAVAZ(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tv-trtavaz.medya.trt.com.tr/master.m3u8","orta5",settings);
+	win.focus();
+}
+function TRTTURK(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tv-trtturk.medya.trt.com.tr/master.m3u8","orta5",settings);
+	win.focus();
+}
+function TRTCOCUK(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tv-trtcocuk.medya.trt.com.tr/master.m3u8","orta5",settings);
+	win.focus();
+}
+function MINIKAGO(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://trkvz-live.daioncdn.net/minikago/minikago.m3u8","orta5",settings);
+	win.focus();
+}
+function MINIKACOCUK(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://trkvz-live.daioncdn.net/minikago_cocuk/minikago_cocuk.m3u8","orta5",settings);
+	win.focus();
+}
+
+function TRTBELGESEL(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tv-trtbelgesel.medya.trt.com.tr/master.m3u8","orta5",settings);
+	win.focus();
+}
+function TGRTBELGESEL(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tvcdn.4t40.workers.dev/https://canlitvulusal.xyz/live/tgrtbelgesel/index.m3u8","orta5",settings);
+	win.focus();
+}
+function SOZCUTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("https://www.youtube.com/embed/ztmY_cCtUl0","orta5",settings);
+	win.focus();
+}
+function HALKTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://halktv-live.daioncdn.net/halktv/halktv.m3u8","orta5",settings);
+	win.focus();
+}
+function KRTTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("https://www.youtube.com/embed/v5srSbD2WBk","orta5",settings);
+	win.focus();
+}
+function TELE1(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tele1-live.ercdn.net/tele1/tele1.m3u8","orta5",settings);
+	win.focus();
+}
+function FLASHHABER(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://flashhaber-live.ercdn.net/flashhaber/flashhaber.m3u8","orta5",settings);
+	win.focus();
+}
+function ULUSALKANAL(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("https://www.youtube.com/embed/dK96AZbCns0","orta5",settings);
+	win.focus();
+}
+function NTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://emmazbvkkf.turknet.ercdn.net/cnifuowzza/ntv/ntv.m3u8","orta5",settings);
+	win.focus();
+}
+function CNNTURK(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tvcdn.4t40.workers.dev/https://tv.arectv11.sbs/live/cnnturk.m3u8","orta5",settings);
+	win.focus();
+}
+function HABERTURK(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://rmtftbjlne.turknet.ercdn.net/bpeytmnqyp/haberturktv/haberturktv.m3u8","orta5",settings);
+	win.focus();
+}
+function HABERGLOBAL(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tv.ensonhaber.com/haberglobal/haberglobal.m3u8","orta5",settings);
+	win.focus();
+}
+function TGRTHABER(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgrthaber-live.daioncdn.net/tgrthaber/tgrthaber.m3u8","orta5",settings);
+	win.focus();
+}
+function TGRTEU(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgrt-live.ercdn.net/tgrteu/tgrteu.m3u8","orta5",settings);
+	win.focus();
+}
+function TRTHABER(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tv-trthaber.medya.trt.com.tr/master.m3u8","orta5",settings);
+	win.focus();
+}
+function TV100(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tv100-live.daioncdn.net/tv100/tv100.m3u8","orta5",settings);
+	win.focus();
+}
+function BEINHABER(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgn.bozztv.com/dvrfl05/gin-beinsportshaber/tracks-v1a1/mono.m3u8","orta5",settings);
+	win.focus();
+}
+function ASPOR(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgn.bozztv.com/dvrfl05/gin-aspor/tracks-v1a1/mono.m3u8","orta5",settings);
+	win.focus();
+}
+function TRTSPOR(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgn.bozztv.com/dvrfl05/gin-trtspor/tracks-v1a1/mono.m3u8","orta5",settings);
+	win.focus();
+}
+function TRTSPOR2(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgn.bozztv.com/dvrfl05/gin-trtspor2/tracks-v1a1/mono.m3u8","orta5",settings);
+	win.focus();
+}
+function TRTSPOR3(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://bozztv.com/gin-dvrfl05/gin-trtspor3/index.m3u8","orta5",settings);
+	win.focus();
+}
+function CBCSPORT(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgn.bozztv.com/dvrfl05/gin-cbcs/tracks-v1a1/mono.m3u8","orta5",settings);
+	win.focus();
+}
+function FBTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgn.bozztv.com/trn03/gt-fbtv/tracks-v1a1/mono.m3u8","orta5",settings);
+	win.focus();
+}
+function TRTMUZIK(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgn.bozztv.com/dvrfl05/gin-trtmusiclive/tracks-v1a1/mono.m3u8","orta5",settings);
+	win.focus();
+}
+
+function TATLISESTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgn.bozztv.com/dvrfl05/gin-powerturk/tracks-v1a1/mono.m3u8","orta5",settings);
+	win.focus();
+}
+function KRALPOP(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgn.bozztv.com/dvrfl05/gin-kralpop/tracks-v1a1/mono.m3u8","orta5",settings);
+	win.focus();
+}
+function POWERTV(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgn.bozztv.com/dvrfl05/gin-powerhd/tracks-v1a1/mono.m3u8","orta5",settings);
+	win.focus();
+}
+function DREAMTURK(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("http://online.hdgo.site/player30/playerjs.html?file=https://tgn.bozztv.com/trn03/gt-dreamturk/tracks-v1a1/mono.m3u8","orta5",settings);
+	win.focus();
+}
+
+
+
+
+// -->
---
+++ 0-Ana-Menu/24-Turis.Saray/tv/turk-tv3/play/js/ch-min.js
@@ -0,0 +1,44 @@
+﻿<!--
+var win=null;
+
+onerror = stopError;
+function stopError(){
+	return true;
+}
+
+function T1(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("htts://saraydorf.de/tv/turk-tv3/ply/tvturk.htm?id=https://www.hdtvlertv.com/","orta7",settings);
+	win.focus();
+
+}
+
+function T2(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("htts://saraydorf.de/spor/spor/tv.html?id=https://tv.ecanlitvizle.video/tv.php","orta7",settings);
+	win.focus();
+
+}
+
+function T3(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("htts://saraydorf.de/tv/turk-tv3/ply/tvcenter.htm?id=https://canlitv.center/canlitv","orta7",settings);
+	win.focus();
+
+}
+
+function T4(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("htts://saraydorf.de/tv/turk-tv3/ply/tvlive.htm?id=https://canlitv.com/trt1-canli","orta7",settings);
+	win.focus();
+
+}
+function T5(){
+	settings="width=100%,height=500,scrollbars=no,location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no,dependent=no";
+	win=window.open("htts://saraydorf.de/tv/turk-tv3/ply/tvocanli.htm?id=https://www.ocanlitv.live/trt-1-izle","orta7",settings);
+	win.focus();
+
+}
+
+
+// -->
