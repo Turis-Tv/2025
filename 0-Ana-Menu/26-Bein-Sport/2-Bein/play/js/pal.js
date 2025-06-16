a295467195a667e6781bb0b2b5a5f33d772e3508
Turis-Tv <turistv3@gmail.com>
2025-06-16 18:00:31 +0200

    Update pal.js

--- 0-Ana-Menu/26-Bein-Sport/2-Bein/play/js/pal.js
+++ 0-Ana-Menu/26-Bein-Sport/2-Bein/play/js/pal.js
@@ -1,7 +1,7 @@
 ﻿      	var url_string = window.location.href;
 var url = new URL(url_string);
 var cid = url.searchParams.get("id"); 
-document.write(" <iframe  id='frame' name='frame' src='//saraydorf.de/spor/spor/tv.html?id=https://www.bosstv4.com/channel?id="+cid+"' width='100%'  height='100%' frameborder='0' allowfullscreen scrolling='no' sandbox='none-popups allow-scripts allow-same-origin'></iframe>");
+document.write(" <iframe  id='frame' name='frame' src='https://saraydorf.de/spor/spor/tv.html?id=https://www.bosstv4.com/channel?id="+cid+"' width='100%'  height='100%' frameborder='0' allowfullscreen scrolling='no' sandbox='none-popups allow-scripts allow-same-origin'></iframe>");
 
 
 
